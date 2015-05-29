27d1ec3960b1a9b9d1e16233268c7c1e2e73dc6e
vrunjeti <vrunjeti@gmail.com>
2015-05-29 00:11:08 -0500

    added ability to refine

--- src/me/hyunbin/colors/Main.java
+++ src/me/hyunbin/colors/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Start Tests");
-        ColorParse.testColorParse(0xFABA02);
-
+        System.out.println("Start Test -----");
+        ColorParse.testColorParse(0xea23bd);
+        System.out.println("End Test -------");
     }
 }
